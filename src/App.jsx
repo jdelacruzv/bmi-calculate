531a619299ff34f2618574d94fdd1d7db2c189bc
Jose De La Cruz <jldlcv@gmail.com>
2022-02-27 18:18:46 -0500

    Finished adding the style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import "./App.css";
+import './App.css';
 
 const App = () => {
 	return (
-		<div className="App">
-			<h1>BMI- Calculate</h1>
+		<div className="app">
+			<header className="app__header">
+				<h1 className="header__title">BMI- Calculate</h1>
+				<span className="toggle">Ligth</span>
+			</header>
+
+			<form className="app__form">
+				<div className="sex">
+					<label className="label__title">Sexo</label>
+					<select className="sex__select" name="gender">
+						<option value="female">Femenino</option>
+						<option value="male">Masculino</option>
+					</select>
+				</div>
+
+				<label className="label__title" htmlFor="weight">
+					Peso (kg)
+				</label>
+				<input className="input__box" type="number" id="weight" value="0" min="0" />
+
+				<label className="label__title" htmlFor="height">
+					Altura (cm)
+				</label>
+				<input className="input__box" type="number" id="height" value="0" min="0" />
+
+				<button className="btn-calculate">Calcular</button>
+			</form>
+
+			<table className="app__table">Resultado</table>
 		</div>
 	);
 };
